c507aaa3ac81864dff664e314e176be39404cd65
James FP <174389827+figgyp@users.noreply.github.com>
2025-08-05 16:09:17 -0400

    get document title from pkg.name

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+import pkg from './package.json';
 
 export default defineConfig({
   envDir: "./config",
+  define: {
+    'import.meta.env.VITE_APP_TITLE': pkg.name ?? "react starter kit",
+  },
   plugins: [
     react(),
     tailwindcss(),
